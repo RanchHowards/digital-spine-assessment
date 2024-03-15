b3c242c84616bdb0118a54010a3bb409b65024c1
Graham <graham.heinz.mccarthy@gmail.com>
2024-03-15 11:58:19 +0100

    add basic types

---
+++ src/types/elevator.ts
@@ -0,0 +1,24 @@
+export interface Elevator {
+  fabricationNumber: string;
+  address: string;
+  floorNumber: number;
+  deviceIdentificationNumber: string;
+  manufacturerName: string;
+  productionYear: number;
+  elevatorType: "Passenger" | "Freight";
+  state: "operational" | "warning" | "out-of-service";
+  chart?: Chart;
+}
+export interface Chart {
+  name: "door_cycle_count_over_time";
+  data: DoorCycleCount[];
+}
+
+export interface DoorCycleCount {
+  time: Date;
+  door_cycles_count: number;
+  door_openings_count: number;
+  door_closings_count: number;
+  door_closed_count: number;
+  door_opened_count: number;
+}
